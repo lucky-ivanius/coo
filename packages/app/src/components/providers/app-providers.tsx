2e973b074647f597f4ff1e5a9f14140cc73a687f
Lucky Ivanius <luckzivanius@gmail.com>
2026-03-16 06:26:47 +0700

    feat: init tanstack query client

--- packages/app/src/components/providers/app-providers.tsx
+++ packages/app/src/components/providers/app-providers.tsx
@@ -1,12 +1,20 @@
+"use client";
+
+import type { StacksNetworkName } from "@stacks/network";
 import type { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 
 import { WalletProvider } from "./wallet-provider";
 
+const queryClient = new QueryClient();
+
 export const AppProviders = ({ children }: PropsWithChildren) => {
   return (
-    <ThemeProvider>
-      <WalletProvider>{children}</WalletProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <WalletProvider network={(process.env.NEXT_PUBLIC_STACKS_NETWORK as StacksNetworkName) ?? "testnet"}>{children}</WalletProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
