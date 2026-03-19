e27cb8d36aadebc2ee0217b5fa297061b76032ef
Lucky Ivanius <luckzivanius@gmail.com>
2026-03-20 02:08:21 +0700

    fix: nextjs env variable rules

--- packages/app/src/lib/stacks-client.ts
+++ packages/app/src/lib/stacks-client.ts
@@ -1,14 +1,14 @@
 import { createClient, StacksApiWebSocketClient } from "@stacks/blockchain-api-client";
 
 export const getStacksClient = () => {
-  const baseUrl = process.env.STACKS_API_BASE_URL || "http://localhost:3999";
+  const baseUrl = process.env.NEXT_PUBLIC_STACKS_API_BASE_URL || "http://localhost:3999";
   const client = createClient({ baseUrl });
 
   return client;
 };
 
 export const getStacksWsClient = () => {
-  const wsUrl = process.env.STACKS_API_WS_URL || "ws://localhost:3999/extended/v1/ws";
+  const wsUrl = process.env.NEXT_PUBLIC_STACKS_WS_URL || "ws://localhost:3999/extended/v1/ws";
   const wsClient = new StacksApiWebSocketClient(new WebSocket(wsUrl));
 
   return wsClient;
